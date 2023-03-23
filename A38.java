5f08937724852a08ba2822988c2ae902913b699c
goyalkunjann <125953017+goyalkunjann@users.noreply.github.com>
2023-03-23 17:34:38 +0530

    Number of Operations to Make Network Connected
    There are n computers numbered from 0 to n - 1 connected by ethernet cables connections forming a network where connections[i] = [ai, bi] represents a connection between computers ai and bi. Any computer can reach any other computer directly or indirectly through the network.
    
    You are given an initial computer network connections. You can extract certain cables between two directly connected computers, and place them between any pair of disconnected computers to make them directly connected.
    
    Return the minimum number of times you need to do this in order to make all the computers connected. If it is not possible, return -1.
    
     
    
    Example 1:
    
    Input: n = 4, connections = [[0,1],[0,2],[1,2]]
    Output: 1
    Explanation: Remove cable between computer 1 and 2 and place between computers 1 and 3.
    
    Example 2:
    
    Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2],[1,3]]
    Output: 2
    
    Example 3:
    
    Input: n = 6, connections = [[0,1],[0,2],[0,3],[1,2]]
    Output: -1
    Explanation: There are not enough cables.
    
     
    
    Constraints:
    
        1 <= n <= 105
        1 <= connections.length <= min(n * (n - 1) / 2, 105)
        connections[i].length == 2
        0 <= ai, bi < n
        ai != bi
        There are no repeated connections.
        No two computers are connected by more than one cable.
    
    Accepted
    140.9K
    Submissions
    232K
    Acceptance Rate
    60.7%

---
+++ A38.java
@@ -0,0 +1,45 @@
+class Solution {
+    public int makeConnected(int n, int[][] connections) {
+        int len = connections.length;
+    if(n > len + 1) return -1;
+
+    UF uf = new UF(n);
+    for(int[] con : connections){
+        uf.union(con[0],con[1]);
+    }
+    return uf.getCount() - 1;
+}
+
+class UF{
+    int[] arr;
+    int count;
+
+    public UF(int n){
+        count = n;
+        arr = new int[n];
+        for(int i = 0; i < n; i++){
+            arr[i] = i;
+        }
+    }
+
+    int find(int a){
+        if(a != arr[a]){
+            arr[a] = find(arr[a]);
+        }
+        return arr[a];
+    }
+
+    boolean union(int a, int b){
+        if(find(a) != find(b)){
+            arr[find(a)] = find(b);
+            count--;
+            return true;
+        }
+        return false;
+    }
+
+    int getCount(){
+        return count;
+    }
+    }
+}
